54fe79e048b3ae597069c3f05c3db5d5b00829d1
Prince Obiuto <152011741+Prince-Obiuto@users.noreply.github.com>
2024-02-05 00:21:36 +0100

    Modified

--- Area of a circle calculator/src/AreaOfACircleCalculator.java
+++ Area of a circle calculator/src/AreaOfACircleCalculator.java
@@ -1,11 +1,11 @@
-/*
- *
- * 
- * 
- * 
- * 
+/*Authors: Uche Peter Onyedikachi 20211286242
+ *Uchendu Samuel Chinaegbomkpam 20211261792
+ * Uche Victory Chinaza 20211264432
+ * Uche Chibueze Favour 20211278052
+ * Udeh Grace Ezekiel 20211293132
+ * Tochukwu Godsfavour Chikamnele 20211272042
  *  
- * Program to calculate the Area of a circle and a sector of 65 degrees.
+ * Program to calculate the Area of a circle and of a sector of 65 degrees.
  */
 
 import java.util.Scanner;
@@ -13,14 +13,20 @@ import java.util.Scanner;
 public class AreaOfACircleCalculator {
     public static void main(String[] args) {
         Scanner radiusInput = new Scanner(System.in);
-        System.out.println("Welcome to group ... assignment!!!");
-        System.out.println("Enter the radius of the circle:");
-        int radius = radiusInput.nextInt();
 
+        // Welcome the user
+        System.out.println("Welcome to group ... assignment!!!");
+        // Prompt for the radius
+        System.out.println("Enter the radius of the circle:");
+        // Capture and store the number
+        int radius = radiusInput.nextInt();
+        
+        // Contants pi and angle value
         double pi = 3.142;
-        double areaOfTheCircle = pi * Math.pow(radius, 2);
         int angleOfTheSector = 65;
-        double areaOfTheSector = (angleOfTheSector / 2) * Math.pow(radius, 2);
+        // Formula to perform the operaation
+        double areaOfTheCircle = pi * Math.pow(radius, 2);
+        double areaOfTheSector = (angleOfTheSector / 360) * pi * Math.pow(radius, 2);
 
         // Print the answers
         System.out.println("The Area of the Circle = " + areaOfTheCircle);
