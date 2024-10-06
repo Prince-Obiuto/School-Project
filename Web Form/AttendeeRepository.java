c1d22ea34517c26832ffeb6c85317635216b9171
Prince Obiuto <152011741+Prince-Obiuto@users.noreply.github.com>
2024-10-06 23:20:47 +0100

    The last changes to deploy before prod. Any new changes would be to resolve anything that breaks during prod or during working. Or to fix bugs

--- Web Form/AttendeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.princeobiuto.demo.repository;
-
-import com.princeobiuto.demo.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttendeeRepository extends JpaRepository<Attendee, Long> {
-}
